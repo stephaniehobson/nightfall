9301cf5c9d57c6862ac55a3cff66603e39ffbca9
Stephanie Hobson <stephaniehobson@users.noreply.github.com>
2026-01-05 20:37:17 -0800

    earlier dark purple

--- nightfall.py
+++ nightfall.py
@@ -256,11 +256,11 @@ weekday_colors = [
         'color': [102, 255, 255] #sky 
     },
     {
-        'time': '18:40:00',
+        'time': '18:30:00',
         'color': [102, 255, 255] #sky
     },
     {
-        'time': '18:45:00',
+        'time': '18:35:00',
         'color': [60, 0, 128] #darkpurple
     },
     {
